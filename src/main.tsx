db47277656794dea9a5fe6b8130a3270b2ac7d3c
otsurob <tulaon0618@gmail.com>
2025-04-25 14:16:12 +0900

    Revert "basenameを追加"
    This reverts commit df633ec67b795704993c6f0c5e7196897537fb8c.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import PoolPage from "./pages/PoolPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/playerPools" element={<PoolPage />} />
