9a9b2b26cd9bdc84cf632b5820903d035fe38754
otsurob <tulaon0618@gmail.com>
2025-04-25 14:16:12 +0900

    Revert "ルーティングの設定を追加"
    This reverts commit 95a0ca454801c132bcdce3be29ff32129dd63275.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import PoolPage from "./pages/PoolPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+    <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/playerPools" element={<PoolPage />} />
